48b366de1d10c7b7d16ef71a87dbbaac63755eab
Matius Febrian Christo <chriss.tkj@gmail.com>
2021-03-18 10:21:01 +0700

    Memperbaiki Dashboard | Done

--- src/store/index.js
+++ src/store/index.js
@@ -1,62 +1,147 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
-// import { resolve } from 'core-js/fn/promise'
+
 
 Vue.use(Vuex)
 
 
 export default new Vuex.Store({
-    state:{
+    state: {
         dataTahunIni: [],
         dataBulanIni: [],
+        detailDataTahunan: [],
+        incomeResult: null,
+        outcomeResult: null,
+        profitResult: null,
+        dataBaru5: null,
         user: null,
         tokenAPI: null,
     },
-    getters:{
+    getters: {
         token: state => {
             // const data = state.tokenAPI;
             return {
                 token_refresh: state.tokenAPI.token_refresh,
                 token_access: state.tokenAPI.token_access
             }
+        },
+        dataBaru: state => {
+            if(state.dataBaru5 !== null){
+
+                return state.dataBaru5.sort((a, b) => {
+                    return parseFloat(a.id) - parseFloat(b.id);
+                }).slice(-5).sort((a, b) => {
+                    return parseFloat(b.id) - parseFloat(a.id)
+                })
+            }
+            
         }
     },
-    actions:{
-       async getData(){
-             const response = await axios.get('administration/administrationdataperyear/');
-            console.log(response)
-        },
-        login({ commit, getters },  form){
+    actions: {
+        getDataTahunIni({
+            commit
+        }) {
             return new Promise((resolve, reject) => {
-                 axios.post('accounts/usertoken/', form)
-                .then( (res)=> {
-                    console.log(res)
-                    console.log(getters)
-                    // console.log(data)
-                    commit('setToken', res)
-                    axios.defaults.headers.common['Authorization'] = 'Bearer ' + getters.token.token_access
-                        resolve(res)
-                    
-                })
-                .catch(err => reject(err))
+                axios.get("administration/administrationdataperyear/")
+                    .then(res => {
+                        const obj = res.data[new Date().getFullYear()];
+                        const hasil = Object.keys(obj).map((key) => [Number(key), obj[key]]);
+                        commit('setDataTahunIni', hasil)
+                        resolve(hasil)
+                    })
+                    .catch(err => reject(err))
             })
-            
+        },
+        getDataBulanIni({
+            commit
+        }, tanggal) {
+            console.log(tanggal)
+            return new Promise((resolve, reject) => {
+                axios.get(`administration/administrationdetail/?year=${tanggal.tahun}&month=${tanggal.bulan}`)
+                    .then(res => {
+                        const data = Object.keys(res.data).map((key) => [
+                            Number(key),
+                            res.data[key],
+                        ]);
+                        commit('setDataBulanIni', data)
+                        resolve(data)
+                    })
+                    .catch(err => reject(err))
+            })
+        },
+        login({
+            commit,
+            getters
+        }, form) {
+            return new Promise((resolve, reject) => {
+                axios.post('accounts/usertoken/', form)
+                    .then((res) => {
+                        console.log(res)
+                        // console.log(data)
+                        commit('setToken', res)
+                        axios.defaults.headers.common['Authorization'] = 'Bearer ' + getters.token.token_access
+                        resolve(res)
+
+                    })
+                    .catch(() => reject())
+            })
+
             // console.log(user, pass)
-            
+
 
         },
-        cekData(state){
-            console.log(state)
+
+        getInOutPro({
+            commit
+        }, data) {
+            commit('getInTahunan', data.income)
+            commit('getOutTahunan', data.outcome)
+            commit('getProTahunan', data.profit)
         },
+
+        getLimaDataBaru({
+            commit
+        }) {
+            new Promise((resolve, reject) => {
+                axios.get("administration/listadministration/")
+                    .then(res => {
+                        commit('getLimaDataBaru', res.data)
+                        resolve(res.data)
+                        
+                        
+                    })
+                    .catch(err => reject(err))
+            })
+                
+            
+        }
+
     },
-    mutations:{
-        setToken(state, token){
+    mutations: {
+        setToken(state, token) {
             state.tokenAPI = {
-                token_refresh : token.data.refresh,
-                token_access : token.data.access
+                token_refresh: token.data.refresh,
+                token_access: token.data.access
             }
-            // console.log(state)
+        },
+        setDataBulanIni(state, data) {
+            state.dataBulanIni = data
+        },
+        setDataTahunIni(state, data) {
+            state.dataTahunIni = data
+        },
+        getInTahunan(state, data) {
+            state.incomeResult = data
+        },
+        getOutTahunan(state, data) {
+            state.outcomeResult = data
+        },
+        getProTahunan(state, data) {
+            state.profitResult = data
+        },
+        getLimaDataBaru(state, data){
+            state.dataBaru5 = data
         }
     }
 })
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -17,9 +17,9 @@
       <!-- ============================================================================================= -->
       <div class="list-data mx-4">
         <DashInfo
-          :income="formatPrice(inTahunIni)"
-          :outcome="formatPrice(outTahunIni)"
-          :profit="formatPrice(proTahunIni)"
+          :income="formatPrice(incomeResult)"
+          :outcome="formatPrice(outcomeResult)"
+          :profit="formatPrice(profitResult)"
         />
       </div>
 
@@ -40,7 +40,7 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(data, index) in dataBaru5" :key="data.id">
+              <tr v-for="(data, index) in dataBaru" :key="data.id">
                 <th scope="row">{{ index + 1 }}</th>
                 <td class="text-wrapper">{{ data.nominal }}</td>
                 <td class="text-wrapper">
@@ -64,45 +64,8 @@
             </tbody>
           </table>
         </div>
-        <div class="col-lg-6 col-12">
-
-        </div>
+        <div class="col-lg-6 col-12"></div>
       </div>
-
-      <!-- <div class="list-data text-dark">
-
-        <div class="row d-flex justify-items-center justify-content-center  m-auto">
-          <div class="col-lg-3 text-center">
-            <div class="list">
-              <h1 class="mt-3">{{ 'Rp. ' + formatPrice(inTahunIni) }}</h1>
-              <h5 class="font-18">Pemasukan Tahun ini</h5>
-            </div>
-              
-          </div>
-          <div class="col-lg-3 text-center">
-              <div class="list">
-              <h1 class="mt-3">10</h1>
-              <h5 class="font-18">Jumlah data bulan ini</h5>
-            </div>
-
-          </div>
-          <div class="col-lg-3 text-center">
-              <div class="list">
-              <h1 class="mt-3">10</h1>
-              <h5 class="font-18">Jumlah data hari ini</h5>
-            </div>
-          </div>
-        </div>
-      </div> -->
-
-      <!-- ============================================================================================= -->
-      <!-- Content -->
-      <!-- Grafik -->
-      <!-- <div class="mx-md-5 mt-2">
-        <div class="col bg-custom line-chart text-light rounded">
-          <line-chart :chart-data="datacollection" :options="chartOption" />
-        </div>
-      </div> -->
     </div>
   </main>
 </template>
@@ -111,14 +74,13 @@
 // @ is an alias to /src
 
 import moment from "moment";
-import axios from "axios";
 import DashInfo from "@/components/DashInfo.vue";
+import { mapActions, mapState, mapGetters } from "vuex";
 
 export default {
   name: "Dashboard",
   props: ["isNav"],
   components: {
-
     DashInfo,
   },
   data() {
@@ -129,11 +91,6 @@ export default {
       outBulanIni: null,
       inBulanIni: null,
       proBulanIni: null,
-      inTahunIni: 0,
-      dataBaru5: null,
-      outTahunIni: 0,
-      proTahunIni: 0,
-      fulldataDaily: null,
       isActiveNav: false,
       isMNavActive: false,
       tanggal: "",
@@ -143,13 +100,28 @@ export default {
     // console.log(typeof localStorage.getItem("login"))
     if (localStorage.getItem("login") === "true") {
       await this.getDataDaily();
-      this.getDataBaru();
-      this.rekapBulanIni();
-      
-    } 
+      this.getLimaDataBaru();
+      // this.rekapBulanIni();
+    }
+  },
+  computed: {
+    ...mapState([
+      "dataTahunIni",
+      "incomeResult",
+      "outcomeResult",
+      "profitResult",
+    ]),
+    ...mapGetters(["dataBaru"]),
   },
 
   methods: {
+    ...mapActions([
+      "getDataTahunIni",
+      "getDataBulanIni",
+      "getInOutPro",
+      "getLimaDataBaru",
+    ]),
+
     CekStatus(value) {
       if (value === "pemasukan") {
         return value;
@@ -182,124 +154,111 @@ export default {
       }
     },
 
-    
+    // rekapBulanIni() {
+    //   const tgl = moment().format("DD-MM-YYYY");
+    //   const tahun = moment(tgl, "DD-MM-YYYY").format("YYYY");
+    //   const bulan = moment(tgl, "DD-MM-YYYY").format("M");
 
-    rekapBulanIni() {
-      const tgl = moment().format("DD-MM-YYYY");
-      const tahun = moment(tgl, "DD-MM-YYYY").format("YYYY");
-      const bulan = moment(tgl, "DD-MM-YYYY").format("M");
-      axios
-        .get(
-          `administration/administrationdetail/?year=${tahun}&month=${bulan}`
-        )
-        .then((res) => {
-          const data = Object.keys(res.data).map((key) => [
-            Number(key),
-            res.data[key],
-          ]);
-          let dataIn;
-          let oldDataIn = null;
-          let dataOut;
-          let oldDataOut = null;
-          for (let i = 0; i < data.length; i++) {
-            if (data[i][1].tipe !== "pemasukan") {
-              if (oldDataOut !== null) {
-                dataOut = data[i][1].nominal;
-                oldDataOut = oldDataOut + dataOut;
-              } else {
-                dataOut = data[i][1].nominal;
-              }
-            } else {
-              if (oldDataOut !== null) {
-                dataIn = data[i][1].nominal;
-                oldDataIn = oldDataIn + dataIn;
-              } else {
-                dataIn = data[i][1].nominal;
-              }
-            }
-          }
+    //   // console.log(tahun)
+    //   this.getDataBulanIni({ bulan, tahun }).then((res) => {
+    //     console.log(res, "q");
+    //     let dataIn;
+    //     let oldDataIn = null;
+    //     let dataOut;
+    //     let oldDataOut = null;
+    //     for (let i = 0; i < res.length; i++) {
+    //       if (res[i][1].tipe !== "pemasukan") {
+    //         if (oldDataOut !== null) {
+    //           dataOut = res[i][1].nominal;
+    //           oldDataOut = oldDataOut + dataOut;
+    //         } else {
+    //           dataOut = res[i][1].nominal;
+    //         }
+    //       } else {
+    //         if (oldDataOut !== null) {
+    //           dataIn = res[i][1].nominal;
+    //           oldDataIn = oldDataIn + dataIn;
+    //         } else {
+    //           dataIn = res[i][1].nominal;
+    //         }
+    //       }
+    //     }
 
-          this.outBulanIni = dataOut;
-          this.inBulanIni = dataIn;
-          this.proBulanIni = dataIn - dataOut;
-        });
-
-        
-    },
+    //     this.outBulanIni = dataOut;
+    //     this.inBulanIni = dataIn;
+    //     this.proBulanIni = dataIn - dataOut;
+    //   });
+    // },
 
     rekapTahunIni() {
       let data = null;
       let dataBaru;
+      let obj = {};
       // Income
-      for (let i = 0; i < this.fulldataDaily.length; i++) {
+      for (let i = 0; i < this.dataTahunIni.length; i++) {
         if (data !== null) {
-          dataBaru = this.fulldataDaily[i].income;
+          dataBaru = this.dataTahunIni[i][1].income;
           data = data + dataBaru;
         } else {
-          data = this.fulldataDaily[i].income;
+          data = this.dataTahunIni[i][1].income;
         }
       }
-      this.inTahunIni = data;
+      obj.income = data;
       //Outcome
       data = null;
-      for (let i = 0; i < this.fulldataDaily.length; i++) {
+      for (let i = 0; i < this.dataTahunIni.length; i++) {
         if (data !== null) {
-          dataBaru = this.fulldataDaily[i].outcome;
+          dataBaru = this.dataTahunIni[i][1].outcome;
           data = data + dataBaru;
         } else {
-          data = this.fulldataDaily[i].outcome;
+          data = this.dataTahunIni[i][1].outcome;
         }
       }
-      this.outTahunIni = data;
+      obj.outcome = data;
       // Profit
       data = null;
-      for (let i = 0; i < this.fulldataDaily.length; i++) {
+      for (let i = 0; i < this.dataTahunIni.length; i++) {
         if (data !== null) {
-          dataBaru = this.fulldataDaily[i].profit;
+          dataBaru = this.dataTahunIni[i][1].profit;
           data = data + dataBaru;
         } else {
-          data = this.fulldataDaily[i].profit;
+          data = this.dataTahunIni[i][1].profit;
         }
       }
-      this.proTahunIni = data;
+      obj.profit = data;
+
+      this.getInOutPro(obj);
     },
     // ================================================
     // Data Daily Report
 
     async getDataDaily() {
-      await axios
-        .get("administration/administrationdataperyear/")
+      await this.getDataTahunIni()
         .then((res) => {
-          const obj = res.data[new Date().getFullYear()];
-          const hasil = Object.keys(obj).map((key) => [Number(key), obj[key]]);
           let bulan;
-          this.dataMonth = hasil.length;
+          this.dataMonth = res.length;
           const income = [];
           const outcome = [];
           const profit = [];
 
-          this.fulldataDaily = [];
-          for (let i = 0; i < hasil.length; i++) {
-            bulan = hasil[i][0];
-            income.push(hasil[i][1].income);
-            outcome.push(hasil[i][1].outcome);
-            profit.push(hasil[i][1].profit);
-            this.fulldataDaily.push({
+          const fulldataDaily = [];
+          for (let i = 0; i < res.length; i++) {
+            bulan = res[i][0];
+            income.push(res[i][1].income);
+            outcome.push(res[i][1].outcome);
+            profit.push(res[i][1].profit);
+            fulldataDaily.push({
               income: income[i],
               outcome: outcome[i],
               profit: profit[i],
-              month: (hasil[i][1].month = moment()
+              month: (res[i][1].month = moment()
                 .locale("id")
                 .month(bulan - 1)
                 .format("MMMM")),
             });
           }
+
           this.rekapTahunIni();
-          console.log(this.inTahunIni);
-          console.log(this.fulldataDaily);
-          // console.log(
-          //   this.filterByValue(this.fullDataChart, "January", "pemasukan", 2000)
-          // );
         })
         .catch((err) => console.log(err));
     },
@@ -307,30 +266,8 @@ export default {
     // Get 5 Data Terbaru
     // ============================================
 
-    async getDataBaru() {
-      await axios
-        .get("administration/listadministration/")
-        .then((res) => {
-          const data = res.data;
-
-          data.sort((a, b) => {
-            return parseFloat(a.id) - parseFloat(b.id);
-          });
-
-          this.dataBaru5 = data.slice(-5);
-
-          this.dataBaru5.sort((a, b) => {
-            return parseFloat(b.id) - parseFloat(a.id)
-          })
-
-          console.log(data)
-        })
-        .catch((err) => console.log(err));
-    },
-
     // Data Chartjs
     // ================================================
-    
   },
 };
 </script>
